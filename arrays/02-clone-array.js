3c18fd26238daae04c50b779adae9b93013e3eb2
veronica <veronicadonoso1506@gmail.com>
2018-10-08 16:05:38 -0400

    02-clone-array two answers

--- arrays/02-clone-array.js
+++ arrays/02-clone-array.js
@@ -4,11 +4,19 @@ console.log(array_Clone([1, 2, 4, 0]));
 console.log(array_Clone([1, 2, [4, 0]]));
 [1, 2, 4, 0]
 [1, 2, [4, 0]] */
-function array_Clone (arr) {
+
+//Option 1
+function arrayClone (arr) {
   for (let i = 0; i < arr.length; i++) {
     let newArrayClone = arr
-    return newArrayClone;
+    return newArrayClone
   }
 }
-console.log(array_Clone([1, 2, 4, 0]))
-console.log(array_Clone([1, 2, [4, 0]]))
+//Option 2
+function arrayClone(arr) {
+  let newArray = [...arr]
+  return newArray
+}
+
+console.log(arrayClone([1, 2, 4, 0]))
+console.log(arrayClone([1, 2, [4, 0]]))
