9de28379dc9b30a4b1e5ffddb4c918adfac515d8
veronica <veronicadonoso1506@gmail.com>
2018-10-27 11:42:10 -0400

    05-joinAll array js exercices done

--- arrays/05-joinAll-array.js
+++ arrays/05-joinAll-array.js
@@ -1,8 +1,11 @@
-/* Write a simple JavaScript program to join all elements of the following array into a string.
+/* 5. Write a simple JavaScript program to join all elements of the following array into a string.
 Sample array : myColor = ["Red", "Green", "White", "Black"];
 Expected Output :
-"Red,Green,White,Black"
+"RedGreenWhiteBlack"
 "Red,Green,White,Black"
 "Red+Green+White+Black" */
-let singTheSong = ['Don\'t worry ', 'about a thing ', 'cause every little thing ', 'gonna be alright'].reduce(function (a, b) { return (a + b)});
-console.log(singTheSong);
+var myColor = ['Red', 'Green', 'White', 'Black']
+
+console.log(myColor.join([',']))
+console.log(myColor.join())
+console.log(myColor.join(['+']))
