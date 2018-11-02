88bb46fd254126b0cde7aa718d9b114d494195d7
veronica <veronicadonoso1506@gmail.com>
2018-11-01 22:39:32 -0400

    09-swapCase array js exercice done

---
+++ arrays/09-swapCase-array.js
@@ -0,0 +1,16 @@
+/* 9. Write a JavaScript program which accept a string as input and swap the case of each character. For example if you input 'The Quick Brown Fox' the output should be 'tHE qUICK bROWN fOX'. */
+var swapCase = function (letters) {
+  var swapEachCharacter = ''
+  for (var i = 0; i < letters.length; i++) {
+    if (letters[i] === letters[i].toUpperCase()) {
+      swapEachCharacter += letters[i].toLowerCase()
+    } else {
+      swapEachCharacter += letters[i].toUpperCase()
+    }
+  }
+  console.log(swapEachCharacter)
+  return swapEachCharacter
+}
+
+var sentences = 'The Quick Brown Fox'
+var swapEachLetter = swapCase(sentences)
