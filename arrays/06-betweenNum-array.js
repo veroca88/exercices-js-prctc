6c3e6df6bdbb3bfa5f401c354e69b0d2dec67203
veronica <veronicadonoso1506@gmail.com>
2018-11-01 23:52:43 -0400

    06-betweenNum array js exercice done

---
+++ arrays/06-betweenNum-array.js
@@ -0,0 +1,16 @@
+/* 6. Write a JavaScript program which accept a number as input and insert dashes (-) between each two even numbers. For example if you accept 025468 the output should be 0-254-6-8. */
+var dashEvenNumber = function (numbers) {
+  var evenNumbers = ''
+  for (var i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0 && numbers[i + 1] % 2 === 0) {
+      evenNumbers += (numbers[i] + '-')
+    } else {
+      evenNumbers += numbers[i]
+    }
+  }
+  console.log(evenNumbers)
+  return evenNumbers
+}
+
+var numbersInput = ('025468')
+var finalNumbers = dashEvenNumber(numbersInput)
