f9d66e93ea2c791b90534c05e8f1eafaae28c420
veronica <veronicadonoso1506@gmail.com>
2018-11-05 21:23:50 -0500

    11-sumSquares array js exercice done

---
+++ arrays/11-sumSquares-array.js
@@ -0,0 +1,8 @@
+/* 11. Write a JavaScript program to find the sum of squares
+of a numeric vector. */
+var sumSquares = function (a, b) {
+  const firstSquare = (a * a)
+  const secondSquare = (b * b)
+  console.log(firstSquare + secondSquare)
+}
+sumSquares(5, -2)
