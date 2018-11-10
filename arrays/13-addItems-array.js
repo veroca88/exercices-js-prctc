ed26369145db029d74f822ea7dbe9c4b77646224
veronica <veronicadonoso1506@gmail.com>
2018-11-09 23:42:38 -0500

    13-addItems array js exercice done

---
+++ arrays/13-addItems-array.js
@@ -0,0 +1,12 @@
+/* 13. Write a JavaScript program to add items in an blank array and display the items.
+Sample Screen : */
+var emptyArr = []
+var itema = 2
+var itemb = 16
+var itemc = 'q'
+
+emptyArr.push(itema)
+emptyArr.push(itemb)
+emptyArr.push(itemc)
+
+console.log(emptyArr)
