59871edeb96c6905548989850161d1d6468a3ca7
veronica <veronicadonoso1506@gmail.com>
2019-02-15 20:36:40 -0500

    15-displayColors-array.js exercise done

--- arrays/15-displayColors-array.js
+++ arrays/15-displayColors-array.js
@@ -5,12 +5,15 @@ Write a JavaScript program to display the colors in the following way :
 "1st choice is Blue ."
 "2nd choice is Green."
 "3rd choice is Red." */
-funcion displayColors (arr) {
-  let newPositions = []
-  for (let i = 0; i < arr.length; i++) {
-    const numbersFirst = i % 10
-    if (numbersFirst) {
-      console.log(i + 'st' + ' choice is ' + arr[i])
+function displayColors (color) {
+  for (let i = 0; i < color.length; i++) {
+    const j = (i + 1) % 10
+    if (j === 1 || j === 2 || j === 3) {
+      console.log((i + 1) + o[j] + ' choice is ' + color[i])
+    } else {
+      console.log((i + 1) + o[0] + ' choice is ' + color[i])
     }
   }
 }
+const o = ['th', 'st', 'nd', 'rd']
+displayColors(['Blue', 'Green', 'Red', 'Orange', 'Violet', 'Indigo', 'Yellow'])
