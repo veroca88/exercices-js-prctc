2416d2730a11a7652233dd66ec385c771ad0d065
veronica <veronicadonoso1506@gmail.com>
2019-02-20 23:01:56 -0500

    In process exercise 19-indexSum-array.js

---
+++ arrays/19-indexSum-array.js
@@ -0,0 +1,15 @@
+/* 19. There are two arrays with individual values, write a JavaScript program to compute the sum of each individual index value from the given arrays.
+Sample array :
+array1 = [1,0,2,3,4];
+array2 = [3,5,6,7,8,13];
+Expected Output :
+[4, 5, 8, 10, 12, 13] */
+function indexSum (array1, array2) {
+  let newSumIndex = 0
+  let value = 0
+  for (var i = 0; i <= array1.length; i++) {
+    return array1[i]
+    }
+  }
+  return newSumIndex
+}
