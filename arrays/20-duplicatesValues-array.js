e2cff1bb65d9cdc8b877b343d7573cbefd04c790
veronica <veronicadonoso1506@gmail.com>
2019-02-25 19:48:13 -0500

    20-duplicatesValues-array.js exercise done

--- arrays/20-duplicatesValues-array.js
+++ arrays/20-duplicatesValues-array.js
@@ -1,1 +1,19 @@
 /* 20. Write a JavaScript program to find duplicate values in a JavaScript array. */
+function findDuplicates (arr) {
+  let object = {}
+  let result = []
+
+  arr.forEach(function (number) {
+    if (!object[number]) { object[number] = 0 }
+    object[number] += 1
+  })
+
+  for (let key in object) {
+    if (object[key] >= 2) {
+      result.push(key)
+    }
+  }
+  return result
+}
+
+console.log(findDuplicates([2, 3, 4, 2, 5, 6, 4, 7, 8, 2, 6, 9]))
